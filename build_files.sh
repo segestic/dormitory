a54a9e3d9a8bd3d698fe1a96440ff897a9e200d0
Olusegun O <50618702+segestic@users.noreply.github.com>
2024-06-20 21:12:11 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
-python3.10 manage.py collectstatic 
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
-python3.10 manage.py migrate accounts
+python3.9 manage.py collectstatic 
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py migrate accounts
 
